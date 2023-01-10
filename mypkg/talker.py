98e1c9d68e41f41140d28c4b0474a36013e7023e
Marin Yasuda <s21C1130TL@s.chibakoudai.jp>
2023-01-10 18:10:59 +0900

    add

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,24 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.srv import Query
 
-class Talker():
-    def __init__(self):
-        self.pub = node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
+def cb(request, response):
+    if request.name == "安田真梨":
+        response.age = 20
+    else:
+        response.age = 255
 
-    def cb(self):
-        msg = Int16()
-        msg.data = self.n
-        self.pub.publish(msg)
-        self.n += 1
+    return response
 
-def main():
-    rclpy.init()
-    node = Node("talker")
-    talker = Talker(node)
-    rclpy.spin(node)
-
-if __name__ == '__main__':
-    main()
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
